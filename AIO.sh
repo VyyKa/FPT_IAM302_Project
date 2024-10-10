823ac40bfbf0e30ef2eb965628fb05532d75e3e8
Huỳnh Ngọc Quang <nquangit@duck.com>
2024-10-10 15:54:34 +0700

    update link for vagrant file

--- AIO.sh
+++ AIO.sh
@@ -238,7 +238,7 @@ setup_capev2_guest_vm() {
     echo -e "${BLUE}Setting up CAPEv2 Guest VM...${NC}"
     
     # wget sample VM Vagrant file
-    wget https://gist.githubusercontent.com/nquangit/83633b69f28757217b1222d112b1a4c3/raw/3dea1f2810a513dfeeade4fa63938ac0d4dc1f0c/Vagrant -O Vagrantfile
+    wget https://gist.githubusercontent.com/nquangit/83633b69f28757217b1222d112b1a4c3/raw/735db383bda2a812f378d2beaa3252d672d04ac4/Vagrant -O Vagrantfile
 
     # Replace the placeholder with the actual values
     sed -i "s/REPLACE_VM_NAME/$VM_NAME/g" Vagrantfile
