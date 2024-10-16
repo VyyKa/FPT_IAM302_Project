871969c38f6fa7b9d72ae8731ef8f1a6af68e05f
Huỳnh Ngọc Quang <nquangit@duck.com>
2024-10-16 14:03:44 +0700

    fix windows path

--- AIO.sh
+++ AIO.sh
@@ -238,7 +238,7 @@ setup_capev2_guest_vm() {
     echo -e "${BLUE}Setting up CAPEv2 Guest VM...${NC}"
     
     # wget sample VM Vagrant file
-    wget https://gist.githubusercontent.com/nquangit/83633b69f28757217b1222d112b1a4c3/raw/36f968361708c797a9f1af04e765503749ebca01/Vagrant -O Vagrantfile
+    wget https://gist.githubusercontent.com/nquangit/83633b69f28757217b1222d112b1a4c3/raw/fed96ce9d46bb803cc1039733382cfea053bb292/Vagrant -O Vagrantfile
 
     # Replace the placeholder with the actual values
     sed -i "s/REPLACE_VM_NAME/$VM_NAME/g" Vagrantfile
