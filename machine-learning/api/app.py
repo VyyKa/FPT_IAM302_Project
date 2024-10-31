1d989ac95b2e39c1291855595ae0747724637d52
Huỳnh Ngọc Quang <nquangit@duck.com>
2024-10-31 17:15:01 +0700

    update machine learning api

--- machine-learning/api/app.py
+++ machine-learning/api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
-# import threading
+import json
+import threading
 
 app = Flask(__name__)
 
@@ -20,17 +21,17 @@ app = Flask(__name__)
 '''
 
 
-@app.route('/ml_task_status', methods=['POST'])
+@app.route('/callback', methods=['POST'])
 def ml_task_status():
     # Get JSON data from the request body
-    data = request.get_json()
+    data = request.data.decode('utf-8')
     if not data or 'task_id' not in data:
-        return jsonify({"error": "task_id is required"}), 400
+        return jsonify({"error": "task_id is required"}), 200
     
-    task_id = data['task_id']
+    task_id = json.loads(data).get('task_id')
     
     # Build the report URL to access task status
-    report_url = f"http://localhost:8000/apiv2/tasks/report/{task_id}"
+    report_url = f"localhost:8000/apiv2/tasks/get/report/{task_id}/"
     
     try:
         # Send a GET request to the report endpoint to fetch the status
@@ -38,19 +39,23 @@ def ml_task_status():
         
         # Check if the request was successful
         if response.status_code == 200:
-            report_data = response.json()
-            status = report_data.get('status', 'Unknown')  # Retrieve the 'status' field from the report
-            return jsonify({"task_id": task_id, "status": status}), 200
+            filename = f"report_{task_id}.json"
 
-            # Call :2 add a thread function to running the machine learning task
+            # Save the report to a file
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+
+            return jsonify({"task_id": task_id}), 200
+
+            # TODO: Call :2 add a thread function to running the machine learning task
 
         else:
-            return jsonify({"error": f"Failed to retrieve report for task_id {task_id}"}), response.status_code
-        
             # TODO: send a request to api_callback_url to update the status of the task to failed
 
+            return jsonify({"error": f"Failed to retrieve report for task_id {task_id}"}), 200
+
     except requests.exceptions.RequestException as e:
-        return jsonify({"error": f"Request to report endpoint failed: {str(e)}"}), 500
+        return jsonify({"error": f"Request to report endpoint failed: {str(e)}"}), 200
 
 if __name__ == '__main__':
     app.run(port=5000)
